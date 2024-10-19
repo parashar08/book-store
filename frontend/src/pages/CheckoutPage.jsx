be87b42f284f0b3d9aec6bb2c100575b19aac013
parashar08 <priyanshuparashar08@gmail.com>
2024-10-19 11:44:55 +0530

    format code

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const CheckoutPage = () => {
-  return (
-    <div>
-      let's checkout your order!
-    </div>
-  )
-}
+    return <div>let's checkout your order!</div>;
+};
 
-export default CheckoutPage
+export default CheckoutPage;
