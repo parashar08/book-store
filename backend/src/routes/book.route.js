2156f2c880b149e794ffbc445784242e3b45ec47
parashar08 <priyanshuparashar08@gmail.com>
2024-10-21 20:35:25 +0530

    all book route

--- backend/src/routes/book.route.js
+++ backend/src/routes/book.route.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import { createBook, getAllBooks } from '../controllers/book.controller.js';
+import {
+    createBook,
+    getAllBooks,
+    getSingleBook,
+    updateBook,
+    deleteBookById,
+} from '../controllers/book.controller.js';
 
 const router = express.Router();
 
 router.route('/create-book').post(createBook);
 router.route('/').get(getAllBooks);
+router.route('/:id', getSingleBook);
+router.route('/update-book/:id', updateBook);
+router.route('/:id').delete(deleteBookById);
 
 export default router;
