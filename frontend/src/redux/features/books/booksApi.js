c33cbf419a5b3106a1af5d49922f6499f26cd509
parashar08 <priyanshuparashar08@gmail.com>
2024-10-23 19:55:47 +0530

    add book api

--- frontend/src/redux/features/books/booksApi.js
+++ frontend/src/redux/features/books/booksApi.js
@@ -18,12 +18,49 @@ const booksApi = createApi({
     baseQuery,
     tagTypes: ['Books'],
     endpoints: (builder) => ({
+        // in get request we use query
         fetchAllBooks: builder.query({
             query: () => '/',
             providesTags: ['Books'],
+        }),
+        fetchBookById: builder.query({
+            query: (id) => `/${id}`,
+            providesTags: (result, error, id) => [{ type: 'Books', id }],
+        }),
+        addBook: builder.mutation({
+            query: (newBook) => ({
+                url: '/create-book',
+                method: 'POST',
+                body: newBook,
+            }),
+            invalidatesTags: ['Books'],
+        }),
+        updateBook: builder.mutation({
+            query: ({ id, ...rest }) => ({
+                url: `update-book/${id}`,
+                method: 'PUT',
+                body: rest,
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }),
+            invalidatesTags: ['Books'],
+        }),
+        deleteBook: builder.mutation({
+            query: (id) => ({
+                url: `/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: ['Books'],
         }),
     }),
 });
 
 export default booksApi;
-export const { useFetchAllBooksQuery } = booksApi;
+export const {
+    useFetchAllBooksQuery,
+    useFetchBookByIdQuery,
+    useAddBookMutation,
+    useUpdateBookMutation,
+    useDeleteBookMutation,
+} = booksApi;
